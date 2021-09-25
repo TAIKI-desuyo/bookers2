a48004073f1b4236290b76d9a8849b0f065b9d1f
TAIKI-desuyo <tyki0673@gmail.com>
2021-09-25 11:18:25 +0000

    [finish]4-10

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,64 @@
+class BooksController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+    @books = Book.all
+    @book = Book.new
+    @user = current_user
+  end
+
+  def show
+    @book = Book.find(params[:id])
+    @user = @book.user
+    @book_new = Book.new
+  end
+
+  def new
+    @book = Book.new
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+    if @book.user == current_user
+      render "edit"
+    else
+      redirect_to books_path
+    end
+  end
+
+   def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @user = current_user
+    @books =Book.all
+    if @book.save
+    flash[:success] = "You have created book successfully."
+    redirect_to book_path(@book.id)
+    else
+    render :index
+    end
+   end
+
+
+  def update
+    @book =Book.find(params[:id])
+  if @book.update(book_params)
+    flash[:success] = "You have updated book successfully."
+    redirect_to book_path(@book.id)
+  else
+    render :edit
+  end
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path
+  end
+
+   private
+
+  def book_params
+    params.require(:book).permit(:title, :body,)
+  end
+end
---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = '9c419a41e6d45eb4cfbe7d6d64416b1baf72b6726c1c04a3e9930960392e152ae0fdf60c4f1c88d48a164c538233c45823d80756c97871873f2ac92c84729617'
