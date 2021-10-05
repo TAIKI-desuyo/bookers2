9ab726bb4124ab7a071c01d53687f7de6739b410
TAIKI-desuyo <tyki0673@gmail.com>
2021-10-05 12:13:43 +0000

    [finish]oyokadai5

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
   get 'home/about' => 'homes#about'
+  get '/search' => 'searchs#search'
+  
   resources :books do
      resource :favorites, only: [:create, :destroy]
      resources :book_comments, only: [:create, :destroy]
